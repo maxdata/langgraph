1dfab4fdebc3269059ee2764a82265ee9d487dde
your_username <your_email@example.com>
2024-07-15 20:10:59 -0400

    c

--- examples/chat_agent_executor_with_function_calling/high-level.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # (Deprecated) Chat Executor: with function calling
-# 
-# The function calling executor is deprecated in favor of [create_react_agent](../chat_agent_executor_with_function_calling/high-level-tools.ipynb).
-# This was done to better align with the underlying model providers' migration from "function calling" to "tool calling", which typically supports parallel tool usage.
-
-# 
